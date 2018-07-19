70541d2527443944d86bdc7f83dd998646ca8e45
sweet_G <ztian2625@163.com>
2018-07-19 21:46:34 +0800

    springMVC运行流程

---
+++ springMVC/src/main/java/com/zt/controller/Controller.java
@@ -0,0 +1,21 @@
+package com.zt.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author zhangtian
+ * @date 2018/7/19
+ */
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+    //@RequestMapping(value = "/hello", method = {RequestMethod.POST,RequestMethod.GET})
+    @GetMapping("/hello")
+    public String hello(){
+        System.out.println("hello springMVC");
+        return "hello";
+    }
+}
---
+++ springMVC/src/main/webapp/WEB-INF/views/hello.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Dream。
+  Date: 2018/7/19
+  Time: 20:43
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <h4>springMVC</h4>
+</body>
+</html>
